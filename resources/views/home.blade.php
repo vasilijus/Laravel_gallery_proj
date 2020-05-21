ee20431e8d453c433f527527bfd0f840da7c3258
vasilijus <vasiliokas@gmail.com>
2020-05-21 23:45:12 +0100

    new home page | added nickname to migration, User, register controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,43 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3">
+            <img src="images/temp.png" class="rounded-circle" style="" />
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>InstaLar</h1>
             </div>
+            <div class="d-flex">
+                <div class="p-5"><b>100</b> posts</div>
+                <div class="p-5"><b>100</b> followers</div>
+                <div class="p-5"><b>100</b> following</div>
+            </div>
+
+            <div class=""><b>instalar.com</b></div>
+            <div class="">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </div>
+            <div class="">
+                <a href="#"><b>instalar.com</b></a>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="images/temp.png" class="rounded-circle" style="" />
+        </div>
+        <div class="col-4">
+            <img src="images/temp.png" class="rounded-circle" style="" />
+        </div>
+        <div class="col-4">
+            <img src="images/temp.png" class="rounded-circle" style="" />
         </div>
     </div>
 </div>
+
 @endsection
