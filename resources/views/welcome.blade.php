595a726b1d011c25fb2aefe285d61ed5cdfd2053
vasilijus <vasiliokas@gmail.com>
2020-05-21 23:06:22 +0100

    composer require laravel/ui 1.0 | ui vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Insta Laravel Project
                 </div>
 
                 <div class="links">
