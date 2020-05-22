1867d98df409b2ae244be51fc85fd66b026a3140
vasilijus <vasiliokas@gmail.com>
2020-05-23 00:10:20 +0100

    Profile belogsTo User | User hasOne Profile .... | vise versa relationships

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    // 1 to 1 relation
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+        // 1 to 1 relation
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+    
 }
